dc324d138a9ccc353cf5cbef04ea6c6ebabd5468
Art38169 <00482@kvis.ac.th>
2026-03-09 12:28:16 +0700

    route controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\CoinGameController;
+use App\Http\Controllers\DiceGameController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 Route::middleware(['auth'])->group(function () {
     Route::view('/dice', 'games.dice')->name('games.dice');
+    Route::post('/dice/play', [DiceGameController::class, 'play'])->name('games.dice.play');
     Route::view('/coin', 'games.coin')->name('games.coin');
+    Route::post('/coin/play', [CoinGameController::class, 'play'])->name('games.coin.play');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
